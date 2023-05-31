6d976e6ec9191e659bb767c27f1bcbb840284643
Dorothy_Akoth <dorothy@untapped-global.com>
2023-05-31 16:47:35 +0300

    accessible variables globaly

--- encapsulation.py
+++ encapsulation.py
@@ -1,12 +1,27 @@
 # Method used to hide some features
 # making our data private in python is by use of double underscore
+# Encapsulation also checks the accessibility of variables
 
 class Hello:
     def __init__(self ,name):
         self.a = 30
         self._b = 40
         self.__c = 50
+class Person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
 
+    def display(self):
+        print(self.name)
+        print(self.age)
+
+
+person = Person("Dory",23)
+# access using class method
+
+person.display()
+# accessing directly from outside
 
 hello = Hello('name')
 print(hello.a)
