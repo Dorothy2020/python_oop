a0ed07ec5aa184288b1d0d280b58b9c81242651d
Dorothy_Akoth <dorothy@untapped-global.com>
2023-06-01 09:02:12 +0300

    polymorphism

---
+++ polymorphism.py
@@ -0,0 +1,18 @@
+# Polymorphism with class methods
+class Kenya():
+    def capital(self):
+        print("The capital City of Kenya is Nairobi ")
+
+    def language(self):
+        print("Swahili is the most spoken language of Kenya")
+
+
+class USA():
+    def capital(self):
+        print("Washington D.C is the capital city of USA")
+
+    def language(self):
+        print("The most spoken language is English")
+
+
+
